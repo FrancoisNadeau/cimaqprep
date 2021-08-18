d815747fd6284bdb009c5703180d4951f05227e9
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-18 15:40:35 -0400

    Update get_outcomes.py

--- get_outcomes.py
+++ get_outcomes.py
@@ -17,19 +17,20 @@ def get_outcomes(behav:pd.DataFrame):
 	 wrong position recognition
 	"false_alarm" = new object misrecognized as old
 	"corr_rejection" = new object recognized as new
-	"miss" = old object misrecognized as new'''
+	"miss" = old object misrecognized as new
+	'''
 	responses = []
-    	for row in behav.iterrows():
-	    	if row[1].recognition_acc and row[1].spatial_acc:
-				responses.append('hit')
-	    	elif row[1].recognition_acc and not row[1].spatial_acc:
-				responses.append('recog_ok_spatial_wrong')
-	    	elif row[1].category == 'new' and row[1].recognition_resp == 'old':
-	        	responses.append('false_alarm')
-	    	elif row[1].category == 'new' and row[1].recognition_resp == 'new':
-				responses.append('corr_rejection')
-	    	elif row[1].category == 'old' and row[1].recognition_resp == 'new':
-				responses.append('miss')
+    for row in behav.iterrows():
+		if row[1].recognition_acc and row[1].spatial_acc:
+			responses.append('hit')
+	    elif row[1].recognition_acc and not row[1].spatial_acc:
+			responses.append('recog_ok_spatial_wrong')
+    	elif row[1].category == 'new' and row[1].recognition_resp == 'old':
+			responses.append('false_alarm')
+		elif row[1].category == 'new' and row[1].recognition_resp == 'new':
+			responses.append('corr_rejection')
+		elif row[1].category == 'old' and row[1].recognition_resp == 'new':
+			responses.append('miss')
     return responses
     
 def main():
