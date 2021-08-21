46136b5a63de2fcdec0031c21cd2e234da5d9109
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-20 22:10:46 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
  
 # __init__.py 
  
-from .clean_resampled_fmri import clean_resampled_fmri 
-from. get_tr_nscans_frametimes import get_tr_nscans_frametimes 
-from.resample_to_smallest import resample_to_smallest 
-from.get_epi_mask_fromdata import get_epi_mask_fromdata 
-from.resample_fmri_to_events import resample_fmri_to_events 
-from.get_outcomes import get_outcomes 
-from.resample_fmri_to_mask import resample_fmri_to_mask
+from .clean_resampled_fmri import clean_resampled_fmri
+from .fetch_scans import fetch_scans
+from .get_tr_nscans_frametimes import get_tr_nscans_frametimes 
+from .resample_to_smallest import resample_to_smallest 
+from .get_epi_mask_fromdata import get_epi_mask_fromdata 
+from .resample_fmri_to_events import resample_fmri_to_events 
+from .get_outcomes import get_outcomes 
+from .resample_fmri_to_mask import resample_fmri_to_mask
 
