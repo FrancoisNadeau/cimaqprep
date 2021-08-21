1da78b8ca964b4334aa9eadf850a9a2c60b19fb6
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-20 22:20:28 -0400

    Update fetch_scans.py

--- fetch_scans.py
+++ fetch_scans.py
@@ -8,10 +8,9 @@ from typing import Union
 def fetch_scans(subject_dir:Union[str,os.PathLike]) -> pd.DataFrame:
 	scans = lu.loadfiles([itm for itm in lu.loadimages(subject_dir)
 						  if not itm.endswith('.json')])
-return df(((grp, scans.groupby('parent').get_group(grp).fpaths.values)
+	return df(((grp, scans.groupby('parent').get_group(grp).fpaths.values)
                              for grp in scans.groupby(
-                                 'parent').groups)).set_index(0).T
-                                 
+                                 'parent').groups)).set_index(0).T                              
 def main():
     fetch_scans(subject_dir)
  
