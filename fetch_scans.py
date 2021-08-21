97f5aff7c9879c4c220f24188610c7f0fd80678f
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-21 15:37:52 -0400

    Update fetch_scans.py

--- fetch_scans.py
+++ fetch_scans.py
@@ -7,6 +7,20 @@ from typing import Union
 import loadutils as lu
 
 def fetch_scans(subject_dir:Union[str,os.PathLike]) -> pd.DataFrame:
+	'''
+	Description
+	-----------
+	Load a participant's scans (nibabel.nifti1.Nifti1Image) files.
+	
+	Parameters
+	----------
+	subject_dir: Path to participant's data directory
+	
+	Returns
+	-------
+	DataFrame containg subject's scans, in the same order as
+	this subject's infos DataFrame (outputed by cimaqprep.fetch_infos)
+	'''
 	scans = lu.loadfiles([itm for itm in lu.loadimages(subject_dir)
 						  if not itm.endswith('.json')])
 	return df(((grp, scans.groupby('parent').get_group(grp).fpaths.values)
