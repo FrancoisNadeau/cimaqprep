3f3c7adbb4da8724b7cb02f981e926c472bc97c4
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-20 22:15:31 -0400

    Add files via upload

---
+++ fetch_infos.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+from pandas import DataFrame as df
+from typing import Union
+
+def fetch_infos(subject_dir:Union[str,os.PathLike]) -> pd.DataFrame:
+	infos = lu.loadfiles([itm for itm in lu.loadimages(subject_dir)
+						  if itm.endswith('.json')])
+    return df(((grp, infos.groupby('parent').get_group(grp).fpaths.values)
+                             for grp in infos.groupby(
+                                 'parent').groups)).set_index(0).T        
+def main():
+    fetch_infos(subject_dir)
+ 
+if __name__ == "__main__":
+    main()
