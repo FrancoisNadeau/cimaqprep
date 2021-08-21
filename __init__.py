5877d51ae90e6bb7b1ff9bd2307ae4e1466a865e
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-21 13:28:40 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@
 # __init__.py 
  
 from .clean_resampled_fmri import clean_resampled_fmri
+from .fetch_events_behav import fetch_events_behav
 from .fetch_infos import fetch_infos
 from .fetch_scans import fetch_scans
 from .get_tr_nscans_frametimes import get_tr_nscans_frametimes 
@@ -11,4 +12,3 @@ from .get_epi_mask_fromdata import get_epi_mask_fromdata
 from .resample_fmri_to_events import resample_fmri_to_events 
 from .get_outcomes import get_outcomes 
 from .resample_fmri_to_mask import resample_fmri_to_mask
-
