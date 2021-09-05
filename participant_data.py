e7943e4e9b85dc774556b3230b22d04786cf38c9
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-09-05 13:18:31 -0400

    Update participant_data.py

--- participant_data.py
+++ participant_data.py
@@ -59,11 +59,9 @@ class participant_data:
         self.events_dir = events_dir
         self.behav_dir = behav_dir
         self.masker_params_dir = masker_params_dir
-        self.data_dir = pjoin(self.cimaq_mar_dir,
-                              'derivatives/CIMAQ_fmri_memory/data/')
+        self.data_dir = pjoin(self.cimaq_mar_dir, 'derivatives/CIMAQ_fmri_memory/data/')
         self.confounds_dir = (pjoin(self.data_dir,'confounds/resample'))
         self.participants_dir = pjoin(self.data_dir, 'participants')
-#         self.atlas_dir = xpu(atlas_dir)
         self.sub_id = fetch_participant(self.cimaq_mar_dir)
         self.mar_scans, self.mar_infos = fetch_scans_infos(pjoin(self.cimaq_mar_dir,self.sub_id[0]))
         self.nov_scans, self.nov_infos = fetch_scans_infos(pjoin(self.cimaq_nov_dir,self.sub_id[1]))
@@ -81,13 +79,6 @@ class participant_data:
             np.arange(0, self.frame_times.max(),
                       self.frame_times.max()/self.events.shape[0])
         # Compute EPI mask
-#         func_img=nib.load(self.mar_scans.func[1][0])
-#         self.mar_epi_mask=nilearn.masking.compute_epi_mask(epi_img=self.mar_scans.fmap[1][-1],
-#                                               connected=True,
-#                                               opening=True,ensure_finite=True,
-#                                               exclude_zeros=True,
-#                                               target_shape=mean_img(func_img).shape,
-#                                               target_affine=mean_img(func_img).affine)
         self.mar_epi_mask = \
             nilearn.masking.compute_epi_mask(epi_img=lu.filterlist_inc(['_epi'], self.mar_scans.fmap[1])[0],
                                              connected=True,
@@ -100,29 +91,6 @@ class participant_data:
                                              verbose=0)
         self.resample_fmri_to_events = resample_fmri_to_events
         self.common_masker_params = lu.read_json(self.masker_params_dir)
-#         self.atlas = nilearn.datasets.fetch_atlas_difumo(dimension=1024,
-#                                                          resolution_mm=3,
-#                                                          data_dir=self.atlas_dir,
-#                                                          resume=True,
-#                                                          verbose=1)
-#         self.maps_masker_params = dict(mask_img=self.mar_epi_mask,
-#                                        maps_img=self.atlas.maps,
-#                                        allow_overlap=True,
-#                                        resampling_target='mask',
-#                                        **self.common_masker_params)
-#         self.maps_masker = \
-#             nilearn.input_data.NiftiMapsMasker(**self.maps_masker_params).fit()
-
-#         self.maps_maps_region_signals = \
-#             self.maps_masker.transform_single_imgs(imgs=self.mar_scans.func[1][0],
-#                                                    confounds=self.confounds)
-#         self.maps_maps_inverse_func = \
-#             self.maps_masker.inverse_transform(region_signals=self.maps_maps_region_signals)
-#         self.maps_elements_region_signals = \
-#             self.maps_masker.transform(imgs=self.mar_scans.func[1][0],
-#                                        confounds=self.confounds)
-#         self.maps_elements_inverse_func = \
-#             self.maps_masker.inverse_transform(region_signals=maps_elements_region_signals)
         self.nifti_masker_params = dict(mask_img=self.mar_epi_mask,
                                         runs=None,
                                         target_affine=self.mar_epi_mask.affine,
@@ -135,65 +103,6 @@ class participant_data:
         self.nifti_masker = \
             nilearn.input_data.NiftiMasker(**self.nifti_masker_params)
 
-#         # Clean functional image using motion and scanner drift confounds
-#         self.clean_func=clean_img(imgs=self.mar_scans.func[1][0],
-#                              sessions=None,
-#                              detrend=False,
-#                              standardize=False,
-#                              confounds=self.confounds,
-#         #                                    low_pass=0.21, high_pass=0.09,
-#                              t_r=self.t_r,
-#                              ensure_finite=True,
-#                              mask_img=self.mar_epi_mask)
-
-        # Resample fMRI image slices to events lenght
-#         decomp_func = df(self.frame_times,columns=['frame_times'])
-#         imgdf=df(zip(self.frame_times,
-#                      list(nilearn.image.iter_img(self.mar_scans.func[1][0]))),
-#                  columns=['frame_times','images'])
-#         test=df(pd.cut(decomp_func['frame_times'], self.events.shape[0]))
-#         test=df(test.frame_times.unique(), columns=['frame_times'])
-#         test['images']=[mean_img(concat_imgs([subrow[1].images for
-#                                               subrow in imgdf.iterrows()
-#                                               if subrow[1].frame_times
-#                                               in row[1].frame_times]))
-#                         for row in tqdm(tuple(test.iterrows()),
-#                                         desc='resampling fMRI image to events lenght')]
-#         self.cleaned_func = concat_imgs(test.images)
-
-        # Compute masker from EPI mask and resampled fMRI image
-#         self.masker = NiftiMasker(mask_img=self.mar_epi_mask,
-#                              mask_strategy='epi',
-#                              runs=None,
-#                              smoothing_fwhm=9.0,
-#                              standardize=True,
-#                              detrend=True,
-#                              high_variance_confounds=True,
-#                              high_pass=0.09,
-#                              low_pass=0.21,
-#                              target_affine=self.mar_epi_mask.affine,
-#                              target_shape=self.mar_epi_mask.shape,
-#                              t_r=self.t_r,
-#                              dtype=float,
-#                              mask_args=dict(opening=True))
-#         self.masker = self.masker.fit(self.cleaned_func)
-#         self.masker_report = self.masker.generate_report()
-#         self.mar_epi_mask = get_epi_mask_fromdata(imgs=self.mar_scans.fmap[1])
-#         self.nov_epi_mask = get_epi_mask_fromdata(imgs=self.nov_scans.fmap[1])
-#         self.resampled_fmri_to_events = \
-#             resample_fmri_to_events(fmri_img=self.mar_scans.func[1][0],
-#                                     mask_img=self.mar_epi_mask,
-#                                     resample_to_mask=True,
-#                                     clean_resampled_imgs=True,
-#                                     **{'confounds':self.confounds,
-#                                        'low_pass':None,
-#                                        'high_pass':None,
-#                                        'detrend':True,
-#                                        'standardize':True,
-#                                        't_r':self.t_r,
-#                                        'ensure_finite':True,
-#                                        'frame_times':self.frame_times,
-#                                        'n_events':self.events})[1]
 def main():
     subject = participant_data(
         cimaq_nov_dir = xpu('~/../../media/francois/seagate_1tb/cimaq_20190901'),
